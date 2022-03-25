1b66abd91d3642bc13b8863f64327ba4224e827c
Mark Wicks <mawicks@gmail.com>
2022-03-24 22:39:39 -0400

    Clarify what History.load() does

--- history_data.py
+++ history_data.py
@@ -71,6 +71,32 @@ class History(object):
         self, symbols: Union[Iterable[str], str], overwrite_existing=False
     ) -> None:
         symbols = utils.to_symbol_list(symbols)
+        """
+        Return a dataframe containing all historic values for the given set of symbosl.
+        The dates are inner joined so there is one row for each date where all symbols
+        have a value for that date.  The row index for the returned dataframe is the
+        date.  The column is a muli-level index where the first position is the symbol
+        and the second position is the value of interest (e.g., "close", "log_return", etc.)
+
+        The expected use case is to get the log returns for a portfolio of stocks.  For example,
+        the following returns a datafram of log returns for a portfolio on the dates where every
+        item in the portfolio has a return:
+
+        df.loc[:, (symbol_list, 'log_return')]
+
+        This is intended for a portfolio, but you can specify just one stock if that's all that's required:
+
+        df.loc[:, (symbol, 'log_return')]
+
+        Arguments:
+           symbols:  Union[Iterable[str], str] - a list of symbols of interst
+           overwrite_existing: bool - whether to overwrite previously downloaded data (default False)
+
+        Returns
+           pd.DataFrame - The column is a muli-level index where the first position is the symbol
+        and the second position is the value of interest (e.g., "close", "log_return", etc.)
+
+        """
 
         self.download(symbols, overwrite_existing)
 
@@ -80,12 +106,16 @@ class History(object):
             df["symbol"] = symbol
             dataframes.append(df)
 
-        return pd.concat(dataframes, axis=0)
+        print(dataframes)
+        return pd.concat(dataframes, axis=1, join="inner", keys=symbols)
 
 
 if __name__ == "__main__":  # pragma: no cover
     cache = FileSystemHistoryCache("training_data")
     data_source = data_sources.YFinanceSource()
     history = History(data_source, cache)
-    df = history.load(["qqq", "spy", "bnd"], overwrite_existing=False)
-    print(df.head())
+    symbols = ["QQQ", "SPY", "BND", "EDV"]
+    df = history.load(symbols, overwrite_existing=False)
+    print(df.loc[:, (symbols, "log_return")])
+
+    print(df.loc[:, (symbols[0], "log_return")])
