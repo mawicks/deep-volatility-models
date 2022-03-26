6b67a9e2c265ce65cf3c5e11c8d9c2ecc7217d1b
Mark Wicks <mawicks@gmail.com>
2022-03-26 11:22:42 -0400

    Refactor time series tests and use parametrize.

--- tests/test_time_series.py
+++ tests/test_time_series.py
@@ -7,135 +7,132 @@ import torch
 import time_series
 
 
-def test_rolling_window_series():
-    d = time_series.RollingWindowSeries(range(10), 3)
-    assert len(d) == 8
-    assert tuple(d[0]) == tuple(range(3))
-    assert tuple(d[1]) == tuple(range(1, 4))
-    assert tuple(d[-1]) == tuple(range(7, 10))
-    assert tuple(d[-2]) == tuple(range(6, 9))
-    assert tuple(d[-8]) == tuple(range(3))
-    with pytest.raises(IndexError):
-        d[8]
-    with pytest.raises(IndexError):
-        d[-9]
-
-    d = time_series.RollingWindowSeries(range(10), 3, stride=2)
-    assert len(d) == 4
-    assert tuple(d[0]) == tuple(range(3))
-    assert tuple(d[1]) == tuple(range(2, 5))
-    assert tuple(d[-1]) == tuple(range(6, 9))
-    assert tuple(d[-2]) == tuple(range(4, 7))
-    assert tuple(d[-4]) == tuple(range(3))
-    with pytest.raises(IndexError):
-        d[4]
-    with pytest.raises(IndexError):
-        d[-5]
-
+def test_rolling_window_arg_check():
     with pytest.raises(ValueError):
         time_series.RollingWindowSeries(range(10), 3, stride=0)
 
 
-def test_target_selection():
-    d = time_series.RollingWindowSeries(range(10), 3, stride=2)
+@pytest.mark.parametrize(
+    "series,window,stride,expected",
+    [
+        (
+            range(10),
+            3,
+            1,
+            [
+                torch.tensor(range(0, 3)),
+                torch.tensor(range(1, 4)),
+                torch.tensor(range(2, 5)),
+                torch.tensor(range(3, 6)),
+                torch.tensor(range(4, 7)),
+                torch.tensor(range(5, 8)),
+                torch.tensor(range(6, 9)),
+                torch.tensor(range(7, 10)),
+            ],
+        ),
+        # Same case with a different stride
+        (
+            range(10),
+            3,
+            2,
+            [
+                torch.tensor(range(0, 3)),
+                torch.tensor(range(2, 5)),
+                torch.tensor(range(4, 7)),
+                torch.tensor(range(6, 9)),
+            ],
+        ),
+        # Check a sequence of vectors
+        (
+            [
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9],
+                [10, 11, 12],
+            ],
+            2,
+            1,
+            [
+                torch.tensor([[1, 4], [2, 5], [3, 6]]),
+                torch.tensor([[4, 7], [5, 8], [6, 9]]),
+                torch.tensor([[7, 10], [8, 11], [9, 12]]),
+            ],
+        ),
+    ],
+)
+def test_rolling_window_series(series, window, stride, expected):
+    d = time_series.RollingWindowSeries(series, window, stride=stride)
+    assert len(d) == len(expected)
+
+    for i in range(len(expected)):
+        assert (d[i] == expected[i]).all()
+
+    for i in range(-len(expected), 0):
+        assert (d[i] == expected[i]).all()
+
+    with pytest.raises(IndexError):
+        d[len(expected)]
+    with pytest.raises(IndexError):
+        d[-len(expected) - 1]
+
+
+@pytest.mark.parametrize(
+    "series,window,stride,expected_cov,expected_target",
+    [
+        (
+            range(10),
+            3,
+            2,
+            [
+                torch.tensor([0, 1]),
+                torch.tensor([2, 3]),
+                torch.tensor([4, 5]),
+                torch.tensor([6, 7]),
+            ],
+            [2, 4, 6, 8],
+        ),
+        # Check a sequence of vectors
+        (
+            [
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9],
+                [10, 11, 12],
+            ],
+            2,
+            1,
+            [
+                torch.tensor([[1], [2], [3]]),
+                torch.tensor([[4], [5], [6]]),
+                torch.tensor([[7], [8], [9]]),
+            ],
+            [
+                torch.tensor([[4], [5], [6]]),
+                torch.tensor([[7], [8], [9]]),
+                torch.tensor([[10], [11], [12]]),
+            ],
+        ),
+    ],
+)
+def test_target_selection(series, window, stride, expected_cov, expected_target):
+    d = time_series.RollingWindowSeries(series, window, stride=stride)
     cts = time_series.ContextAndTargetSeries(d, target_dim=1)
 
-    assert len(d) == 4
-    assert len(cts) == 4
+    assert len(cts) == len(expected_target)
 
-    expected_cov = [
-        torch.tensor([0, 1]),
-        torch.tensor([2, 3]),
-        torch.tensor([4, 5]),
-        torch.tensor([6, 7]),
-    ]
-
-    expected_target = [2, 4, 6, 8]
-
-    for i in range(4):
+    for i in range(len(expected_target)):
         cov, target = cts[i]
         assert (cov == expected_cov[i]).all()
-        assert target == expected_target[i]
+        assert (target == expected_target[i]).all()
 
     # Make sure negatives indexes work
-    for i in range(-4, 0):
+    for i in range(-len(expected_target), 0):
         cov, target = cts[i]
         assert (cov == expected_cov[i]).all()
-        assert target == expected_target[i]
+        assert (target == expected_target[i]).all()
 
     with pytest.raises(IndexError):
-        cts[4]
+        cts[len(expected_target)]
 
     with pytest.raises(IndexError):
-        cts[-5]
-
-
-def test_vector_series():
-    series = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-        [10, 11, 12],
-    ]
-    # Output sequence (represented as np arrays) should be
-    expected = [
-        torch.tensor([[1, 4], [2, 5], [3, 6]]),
-        torch.tensor([[4, 7], [5, 8], [6, 9]]),
-        torch.tensor([[7, 10], [8, 11], [9, 12]]),
-    ]
-
-    d = time_series.RollingWindowSeries(series, 2)
-    for i in range(3):
-        print(f"\nExpected:\n{expected[i]}")
-        print(f"Got:\n{d[i]}")
-        assert (d[i] == expected[i]).all().all()
-
-    # Make sure negative indexes work
-    for i in range(-3, 0):
-        print(f"index: {i}")
-        assert (d[i] == expected[i]).all().all()
-
-    with pytest.raises(IndexError):
-        d[3]
-
-    with pytest.raises(IndexError):
-        d[-4]
-
-    cts = time_series.ContextAndTargetSeries(d, target_dim=1)
-
-    expected_cov = [
-        torch.tensor([[1], [2], [3]]),
-        torch.tensor([[4], [5], [6]]),
-        torch.tensor([[7], [8], [9]]),
-    ]
-
-    expected_target = [
-        torch.tensor([[4], [5], [6]]),
-        torch.tensor([[7], [8], [9]]),
-        torch.tensor([[10], [11], [12]]),
-    ]
-
-    for i in range(3):
-        cov, target = cts[i]
-        print(f"\nExpected cov:\n{expected_cov[i]}")
-        print(f"Got cov:\n {cov}")
-
-        print(f"\nExpected target:\n{expected_target[i]}")
-        print(f"Got target:\n{target}")
-
-        assert (cov == expected_cov[i]).all().all()
-        assert (target == expected_target[i]).all().all()
-
-        # Make sure negative indexes work
-
-    for i in range(-3, 0):
-        print(f"index: {i}")
-        cov, target = cts[i]
-        assert (cov == expected_cov[i]).all().all()
-        assert (target == expected_target[i]).all().all()
-
-    with pytest.raises(IndexError):
-        cts[3]
-
-    with pytest.raises(IndexError):
-        cts[-4]
+        cts[-len(expected_target) - 1]
