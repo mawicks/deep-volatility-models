b25e5c6dcdebaffcc13365dd53561ef82907c73d
Mark Wicks <mawicks@gmail.com>
2022-03-25 23:27:40 -0400

    More work on docstrings.

--- time_series.py
+++ time_series.py
@@ -4,23 +4,25 @@ import torch.utils.data
 
 class RollingWindowSeries(torch.utils.data.Dataset):
     """
-    Given a time series, return a sequence of rolling windows on the series.
-    This generates windows compatible for pytorch dataloader, so the kth window
-    is obtained by indexing the kth element of the output series.  Also for compatibility
-    with pytorch, the output is represented as pytorch tensors.
+    Given a time series, construct a sequence of rolling windows on the series.
+    The resuling windows are compatible with the pytorch dataloader: the kth
+    window is obtained by indexing the kth element of the output series.  Also,
+    for compatibility with pytorch the output is represented by pytorch tensors
+    and it follows pytorch conventions dimension order as explained below.
 
     Example usage:
+
     >>> import time_series
 
-    This modules works with either scalar time series or vector time series.
-    The first example is a sequence of scalars:
+    This modules works with time seris of scalars or time series of vectors. The
+    first example is a sequence of scalars:
 
     >>> series = list(range(5))
     >>> series
     [0, 1, 2, 3, 4]
 
-    Construct a rolling sequence of windows for the series with a window size of 3
-    and a default stride of 1.
+    Construct a rolling sequence of windows for the series with a window size of
+    3 and a default stride of 1.
 
     >>> windowed_series = time_series.RollingWindowSeries(series, 3)
 
@@ -39,12 +41,13 @@ class RollingWindowSeries(torch.utils.data.Dataset):
     >>> windowed_series[2]
     tensor([2., 3., 4.])
 
-    RollingWindowSeries also works for vector-valued time series if you following
-    some conventions about the ordering of dimensions.  We assume that the first
-    dimension of the input (dimension 0) represents time since it's a sequence
-    of vectors.  This is a natural convention for the input sequence, however,
-    we follow the pytorch convention on the output.  The pytorch convention is
-    that the *last* dimension represents time.
+    RollingWindowSeries also works for vector-valued time series as long as you
+    understand some conventions about the ordering of dimensions.  We assume
+    that the first dimension of the input (dimension 0) represents time.  In
+    other words, we assume the input is a sequence of vectors.  This is a
+    natural convention for the input sequence.  However, we follow the pytorch
+    convention on the output.  The pytorch convention is that the *last*
+    dimension represents time.
 
     An example will clarify these ideas.
 
@@ -55,40 +58,41 @@ class RollingWindowSeries(torch.utils.data.Dataset):
             [2., 4., 6.]])
 
     The result may seem "transposed", but that's for consistency with pytorch
-    conventions used in a number of functions.  Here's the rationale.  For
-    a sequence of vectors, the vector dimension should be thought of as the
-    "depth" dimension (e.g., RGB for images). The pytorch convention is for
-    the depth to be the first dimension (dimension 0) of the tensor and for
-    the "time" (or space) dimension to be dimension 1 for 1d or dimensions 1
-    and 2 for 2d.  When these records get batched for machine learning, the index
-    of the record is always dimension 0, so the depth becomes dimension 1,
-    and "time" becomes dimension 2.  The convention for batched records is
-    typically as follows:
+    conventions and necessary for use with a number of pytorch functions. Here's
+    the rationale.  For a sequence of vectors, the vector dimension should be
+    thought of as the "depth" dimension (e.g., RGB for images). The pytorch
+    convention is for the depth to be the first dimension dimension 0) of the
+    tensor and for the "time" (or space) dimension to be dimension 1 for 1d or
+    dimensions 1 and 2 for 2d.  When these ecords get batched for machine
+    learning, the index of the record is always dimension 0, so the depth
+    becomes dimension 1, and "time" becomes dimension 2.  The convention for
+    batched records is typically as follows:
 
-       dimension 0 - index of record within a batch
-       dimension 1 - "depth" dimension
-       dimension 2 - "time" dimension for 1d or "x" dimensions for 2d
+       dimension 0 - index of record within a batch dimension 1 - "depth"
+       dimension dimension 2 - "time" dimension for 1d or "x" dimensions for 2d
        dimension 3 - "y" dimension for 2d
 
-    Since we're looking at records before they have been batched, the convention is
+    Since we're looking at records before they have been batched, the convention
+    is
 
-       dimension 0 - "depth" dimension
-       dimension 1 - "time" dimension for 1d or "x" dimensions for 2d
-       dimension 2 - "y" dimension for 2d
+       dimension 0 - "depth" dimension dimension 1 - "time" dimension for 1d or
+       "x" dimensions for 2d dimension 2 - "y" dimension for 2d
 
-    More generally, the pytorch convention for time series (or any 1d signal)
-    is that time (or whatever the 1d dimension represents) should always be
-    the *last* dimension.  For images, "x" and "y" should be the last
-    *two* dimensions.
-    Continuing the exmaple, here's the next window:
+    More generally, the pytorch convention for time series (or any 1d signal) is
+    that time (or whatever the 1d dimension represents) should always be the
+    *last* dimension.  For images, "x" and "y" should be the last *two*
+    dimensions. Continuing the exmaple, here's the next window:
 
     >>> windowed_vector_series[1]
     tensor([[3., 5., 7.],
             [4., 6., 8.]])
 
-    Note: This code currently works for sequences of scalars and sequences of 1d vectors.
-    TODO: Make this code work for sequences of tensors with two or more diemsions while following
-    the above conventions that "time" should be the last dimension.
+    Note: This code currently works for sequences of scalars and sequences of 1d
+    vectors.
+
+    TODO: Make this code work for sequences of tensors with two or more
+    diemsions while following the above conventions that "time" should be the
+    last dimension.
     """
 
     def __init__(self, series, sequence_length, stride=1):
