e9404bd99829a409950f276e47b792de8f935b25
Mark Wicks <mawicks@gmail.com>
2022-03-29 22:15:06 -0400

    Add separate univariate loss function as a special case.

--- loss_functions.py
+++ loss_functions.py
@@ -1,13 +1,12 @@
 # Standard Python
 import math
+
 # Common packages
 import torch
 
 LOG_SQRT_TWO_PI = 0.5 * math.log(2.0 * math.pi)
-EPS = 1e-8
-SINGULAR_EPS = 1e-4
-
-MAX_SIGMA_ENTRY = torch.tensor([1.0])
+EPS_FOR_LOG = 1e-8
+EPS_FOR_SINGULARITY = 1e-4
 
 # TODO: Write a test for this
 
@@ -19,9 +18,9 @@ def ensure_nonsingular(x):
     to make the minimum change to reach the threshhold.
     """
     d = torch.diagonal(x, dim1=2, dim2=3)
-    pos = ((d < SINGULAR_EPS) * (d >= 0.0)).type(torch.float32)
-    neg = ((d > -SINGULAR_EPS) * (d < 0.0)).type(torch.float32)
-    perturbation = pos * (SINGULAR_EPS - d) + neg * (-SINGULAR_EPS - d)
+    pos = ((d < EPS_FOR_SINGULARITY) * (d >= 0.0)).type(torch.float32)
+    neg = ((d > -EPS_FOR_SINGULARITY) * (d < 0.0)).type(torch.float32)
+    perturbation = pos * (EPS_FOR_SINGULARITY - d) + neg * (-EPS_FOR_SINGULARITY - d)
     return x + torch.diag_embed(perturbation)
 
 
@@ -37,156 +36,24 @@ def combine_mixture_metrics(p, mu, sigma):
         gain (tensor(mb_size)): expected gain (in log-normal space)
         std_dev (tensor(mb_size)): std deviations (in log-normal sape)
     """
-    sigma_squared = sigma**2
+    sigma_squared = sigma ** 2
     component_means = torch.exp(mu + 0.5 * sigma_squared)
-    component_variances = (torch.exp(sigma_squared) - 1.0) * component_means**2
+    component_variances = (torch.exp(sigma_squared) - 1.0) * component_means ** 2
     mean = torch.sum(p * component_means, dim=1)
     # TODO: Verify this is correct implementation of parallel axis theorem
-    shifted_component_variances = (component_variances +
-                                   (mean.unsqueeze(1) - component_means)**2)
+    shifted_component_variances = (
+        component_variances + (mean.unsqueeze(1) - component_means) ** 2
+    )
     std_dev = torch.sqrt(torch.sum(p * shifted_component_variances, dim=1))
     return mean - 1.0, std_dev
 
 
-def get_portfolio_truth(allocation, true_returns):
-    x = torch.log(torch.sum(allocation * torch.exp(true_returns), dim=1))
-    return x.unsqueeze(1)
-
-
-def check(t, name):
-    if torch.isnan(t).any():
-        print('got nan')
-        print(name, t)
-        return True
-    if torch.isinf(t).any():
-        print('got inf')
-        print(name, t)
-        return True
-    return False
-
-
-def get_portfolio_distribution(allocation, mu, sigma_inv):
-    """
-    Given the assumed allocation, reduce the multivariate log-normal
-    distributions characterized by mu and sigma_inv to a univariate
-    log-normal mixture representing the entire portfolio
-
-    Inputs:
-       allocation (tensor(mb_size, channels): Allocation among channels
-       mu (tensor(mb_size, mixture_components, channels): mu for each component
-       sigma_inv (tensor(mb_size, mixture_components, channels, channels)):
-           - sqrt of inverse of covariance matrix for each component
-             (More specifically, the inverse of the lower triangular
-             Cholesky factor of the channel covariances so that
-             C^{-1} = L^T L)
-
-    Outputs:
-       mu (tensor(mb_size, mixture_components, 1))
-       sigma_inv (tensor(mb_size, mixture_components, 1, 1))
-
-    """
-    mb_size, mixture_components, channels = mu.shape
-    check(mu, 'mu')
-    check(sigma_inv, 'sigma_inv')
-
-    # Duplicate the allocation across all of the components by
-    #    1) Creating a component dimension with unsqueeze(1)
-    #    2) Duplicating the contents with expand
-    # Discard the originally passed in allocation
-
-    allocation = allocation.unsqueeze(1).expand(
-        (mb_size, mixture_components, channels))
-
-    sigma = torch.inverse(ensure_nonsingular(sigma_inv))
-    check(sigma, 'sigma')
-    # Limit sigma before computing covariance since limiting
-    # entries of covariance might not preserve positive definiteness
-    sigma = torch.max(torch.min(sigma, MAX_SIGMA_ENTRY), -MAX_SIGMA_ENTRY)
-
-    # covariance is (mb_size, mixture_components, channels, channels)
-    # and has the full covariance matrix for each batch element and
-    # mixture component
-    covariance = torch.matmul(sigma, torch.transpose(sigma, 2, 3))
-    check(covariance, 'covariance')
-
-    # marginal_variances is (mb_size, mixture_components, channels) and
-    # has the channels maginal_variances for each batch element and
-    # mixture component
-    marginal_variances = torch.diagonal(covariance, dim1=2, dim2=3)
-    check(marginal_variances, 'marginal_variances')
-
-    # mu_z is the mean for the log-normal distribution (whereas mu
-    # is the mean for the underlying normal distribution)
-    mu_z = torch.exp(mu + 0.5 * marginal_variances)
-    if check(mu_z, 'mu_z'):
-        print('mu', mu)
-        print('marginal_variances', marginal_variances)
-        print('sigma', sigma)
-        print('sigma_inv', sigma_inv)
-
-    # diag_mu_z contains matrices with mu_z along the diagonal
-    diag_mu_z = torch.diag_embed(mu_z)
-    check(diag_mu_z, 'diag_mu_z')
-
-    # covariance_z is (mb_size, mixture_components, channels,
-    # channels) and represents the covariance of the log-normal
-    # distribution (whereas covariance is the covariance for the
-    # underlying normal distribution)
-    tec = torch.exp(covariance)
-    check(tec, 'tec')
-
-    tec1 = tec - 1.0
-    check(tec1, 'tec1')
-
-    t1 = torch.matmul(diag_mu_z, tec1)
-    if check(t1, 't1'):
-        print('diag_mu_z', diag_mu_z)
-        print('tec1', tec1)
-
-    covariance_z = torch.matmul(t1, diag_mu_z)
-    check(covariance_z, 'covariance_z')
-
-    # mu_p_z is (mb_size, mixture_components, 1) and contains the
-    # expected value of the porfolio's log-normal distribution for
-    # each batch element and mixture component
-    mu_p_z = torch.sum(allocation * mu_z, dim=2).unsqueeze(2)
-    check(mu_p_z, 'mu_p_z')
-
-    # variance_p_z is (mb_size, mixture_components, 1, 1) and contains the
-    # variance of the porfolio's log-normal distribution for
-    # each batch element and mixture component
-    variance_p_z = torch.matmul(
-        allocation.unsqueeze(2),
-        torch.matmul(covariance_z, allocation.unsqueeze(3)))
-    check(variance_p_z, 'variance_p_z')
-
-    # mu_p_x is (mb_size, mixture_components, 1) and contains the
-    # expected value of the porfolio's log-normal distribution for
-    # each batch element and mixture component
-    variance_p_x = torch.log(1.0 + variance_p_z.squeeze(3) / mu_p_z**2)
-    check(variance_p_x, 'variance_p_x')
-
-    mu_p_x = torch.log(mu_p_z + EPS) - 0.5 * variance_p_x
-    check(mu_p_x, 'mu_p_x')
-
-    sigma_inv_p_x = 1 / torch.sqrt(variance_p_x.unsqueeze(3) + EPS)
-    if check(sigma_inv_p_x, 'sigma_inv_p_x'):
-        print('variance_p_x', variance_p_x)
-        print('tec1', tec1)
-        print('diag_mu_z', diag_mu_z)
-        print('covariance_z', covariance_z)
-        print('covariance', covariance)
-        print('variance_p_z', variance_p_z)
-
-    return mu_p_x, sigma_inv_p_x
-
-
 def multivariate_stats(x):
     """Inputs:
-         x (tensor(mb_size, channels)): values
-       Outputs:
-         mu (tensor(channels)): channel means
-         sigma (tensor(channels, channels)): cholesky factor of cov matrix
+      x (tensor(mb_size, channels)): values
+    Outputs:
+      mu (tensor(channels)): channel means
+      sigma (tensor(channels, channels)): cholesky factor of cov matrix
     """
     mb_size, channels = x.shape
     mu = torch.mean(x, dim=0)
@@ -202,6 +69,47 @@ def multivariate_stats(x):
     # Return cholesky factor
     sigma = torch.cholesky(cov)
     return mu, sigma
+
+
+def univariate_mixture_log_likelihood(x, log_p, mu, sigma_inv):
+    """
+    Inputs:
+       x: tensor of shape tensor(mb_size,) containing the observed values
+
+       log_p: tensor of shape (mb_size, mixture_componente) containing the log
+       probability of each component.
+
+       mu: tensor of shape (mb_size, mixture_components) containing the mu
+       estimate for each component
+
+       sigma_inv: tensor of shape (mb_size, mixture_components) containing the
+       estimate of the reciprocal of the sqrt of the variance for each component
+
+    Output:
+       tensor of shape (mb_size,) containing the log likelihood for each sample
+       in the batch
+    """
+    mb_size, mixture_components = log_p.shape
+    if x.shape[0] != mb_size or mu.shape != log_p.shape or mu.shape != sigma_inv.shape:
+        raise ValueError("Dimensions of x, log_p, mu, and sigma_inv are inconsistent")
+
+    # Subtract mu from x in each component.
+    # Be explicit rather than relying on broadcasting
+    e = x.unsqueeze(1).expand(mu.shape) - mu
+
+    z_squared = (sigma_inv * e) ** 2
+
+    # Inclusion of EPS is to ensure argument remains bounded away from zero.
+    log_sigma_inv = torch.log(EPS_FOR_LOG + torch.abs(sigma_inv))
+
+    # log_p, z_squared, and log_sigma_inv have the same shape: (mb_size, mixture_components)
+
+    ll_components = log_p - 0.5 * z_squared + log_sigma_inv - LOG_SQRT_TWO_PI
+
+    # Now sum over the mixture components with logsumexp to get the liklihoods
+    # for each batch sample
+    ll = torch.logsumexp(ll_components, dim=1)
+    return ll
 
 
 def multivariate_mixture_log_likelihood(x, log_p, mu, sigma_inv):
@@ -238,7 +146,7 @@ def multivariate_mixture_log_likelihood(x, log_p, mu, sigma_inv):
     z = torch.matmul(sigma_inv, e)
     # z is (mb_size, mixture_components, channels, 1)
 
-    z_squared = torch.sum((z**2).squeeze(3), dim=2)
+    z_squared = torch.sum((z ** 2).squeeze(3), dim=2)
     # z_squared is (mb_size, mixture_components)
 
     # print('x: ', x)
@@ -249,7 +157,8 @@ def multivariate_mixture_log_likelihood(x, log_p, mu, sigma_inv):
     # Compute the log of the diagonal entries of the inverse covariance matrix
     # Inclusion of EPS is to ensure argument stays well above zero.
     log_diag_sigma_inv = torch.log(
-        EPS + torch.abs(torch.diagonal(sigma_inv, 0, -2, -1)))
+        EPS_FOR_LOG + torch.abs(torch.diagonal(sigma_inv, 0, -2, -1))
+    )
     # log_diag_sigma_inv is (mb_size, mixture_components, channels)
 
     # Compute the log of the determinant of the inverse covariance
@@ -258,8 +167,9 @@ def multivariate_mixture_log_likelihood(x, log_p, mu, sigma_inv):
     # print('log_det_sigma_inv', log_det_sigma_inv)
     # log_det_sigma_inv is (mb_size, mixture_components)
 
-    ll_components = (log_p - 0.5 * z_squared + log_det_sigma_inv -
-                     channels * LOG_SQRT_TWO_PI)
+    ll_components = (
+        log_p - 0.5 * z_squared + log_det_sigma_inv - channels * LOG_SQRT_TWO_PI
+    )
 
     # Now sum over the components with logsumexp to get the liklihoods
     # for each batch sample
---
+++ tests/test_loss.py
@@ -0,0 +1,202 @@
+import pytest
+import torch
+import loss_functions
+import math
+import numpy as np
+
+BATCH_SIZE = 16
+
+softmax = torch.nn.Softmax(dim=1)
+logsoftmax = torch.nn.LogSoftmax(dim=1)
+
+
+def test_multivariate_likelihood():
+    """
+    `    This only check that multivariate_mixture_log_likelihood can be called
+        with arguments of consistent dimension within the correct range.
+    """
+    for batch_size in range(BATCH_SIZE, BATCH_SIZE + 2):
+        for mixture in range(1, 3):
+            for channels in range(1, 3):
+                x = torch.randn(batch_size, channels)
+                log_p = logsoftmax(torch.randn(batch_size, mixture))
+                mu = torch.randn(batch_size, mixture, channels)
+                sigma_inv = torch.tril(
+                    torch.randn(batch_size, mixture, channels, channels)
+                )
+
+                log_loss = loss_functions.multivariate_mixture_log_likelihood(
+                    x, log_p, mu, sigma_inv
+                )
+
+                assert log_loss.shape == (batch_size,)
+                assert float(torch.sum(log_loss)) != 0.0
+
+
+LOG_SQRT_TWO_PI = 0.5 * math.log(2.0 * math.pi)
+# TODO: Why can't this be smaller?
+EPS = 1e-7
+
+
+@pytest.mark.parametrize(
+    "x,log_p,mu,sigma_inv,expected",
+    [
+        # Case 0
+        (
+            torch.ones((1, 1)),
+            0 * torch.ones((1, 1)),
+            torch.ones((1, 1, 1)),
+            torch.ones((1, 1, 1, 1)),
+            -LOG_SQRT_TWO_PI,
+        ),
+        # Case 1
+        (
+            torch.ones((1, 1)),
+            0 * torch.ones((1, 1)),
+            torch.ones((1, 1, 1)),
+            2.0 * torch.ones((1, 1, 1, 1)),
+            math.log(2.0) - LOG_SQRT_TWO_PI,
+        ),
+        # Case 2
+        (
+            torch.ones((1, 1)),
+            0 * torch.ones((1, 1)),
+            0 * torch.ones((1, 1, 1)),
+            2.0 * torch.ones((1, 1, 1, 1)),
+            math.log(2.0) - LOG_SQRT_TWO_PI - 2.0,
+        ),
+        # Case 3
+        (
+            0 * torch.ones((1, 1)),
+            0 * torch.ones((1, 1)),
+            torch.ones((1, 1, 1)),
+            2.0 * torch.ones((1, 1, 1, 1)),
+            math.log(2.0) - LOG_SQRT_TWO_PI - 2.0,
+        ),
+    ],
+)
+def test_multivariate_likelihood_cases(x, log_p, mu, sigma_inv, expected):
+    log_loss = loss_functions.multivariate_mixture_log_likelihood(
+        x, log_p, mu, sigma_inv
+    )
+    assert float(log_loss) == pytest.approx(expected, EPS)
+
+    log_loss = loss_functions.univariate_mixture_log_likelihood(
+        x.squeeze(1), log_p, mu.squeeze(2), sigma_inv.squeeze(3).squeeze(2)
+    )
+    assert float(log_loss) == pytest.approx(expected, EPS)
+
+
+@pytest.mark.parametrize(
+    "mb_size, mixture_components",
+    [
+        # Case 0
+        (7, 3),
+        (11, 10),
+    ],
+)
+def test_combine_mixture_metrics_random(mb_size, mixture_components):
+    """
+    Check that combine_mixture_metrics runs without failure on
+    random inputs
+    """
+    p = softmax(torch.randn((mb_size, mixture_components)))
+    mu = 0.001 + 0.0001 * torch.randn((mb_size, mixture_components))
+    sigma = 0.01 * torch.exp(torch.randn((mb_size, mixture_components)))
+
+    gain, std_dev = loss_functions.combine_mixture_metrics(p, mu, sigma)
+
+    print(gain)
+    print(std_dev)
+
+    assert gain.shape == (mb_size,)
+    assert std_dev.shape == (mb_size,)
+
+
+def test_combine_mixture_metrics_specific():
+    """Check that combine_mixture_metrics() returns correct results on
+    a specific test case.
+    """
+    p = torch.tensor([[1.0, 0.0], [0.75, 0.25], [0.5, 0.5], [0.25, 0.75], [0.0, 1.0]])
+
+    mu_base = np.array([0.001, 0.0005], dtype=np.float32)
+    sigma_base = np.array([0.01, 0.005], dtype=np.float32)
+
+    mu = torch.tensor(mu_base).unsqueeze(0).expand((len(p), len(mu_base)))
+    sigma = torch.tensor(sigma_base).unsqueeze(0).expand((len(p), len(sigma_base)))
+
+    print("p: ", p)
+    print("mu: ", mu)
+    print("sigma: ", sigma)
+
+    gain_test, std_dev_test = loss_functions.combine_mixture_metrics(p, mu, sigma)
+
+    print("gain_test: ", gain_test)
+    print("std_dev_test: ", std_dev_test)
+
+    # Compute the expected values using numpy functions
+    component_means = np.exp(mu_base + 0.5 * sigma_base ** 2)
+    component_variances = (np.exp(sigma_base ** 2) - 1.0) * component_means ** 2
+
+    print("component means:", component_means)
+    print("component variances: ", component_variances)
+
+    mean_expected = []
+    std_dev_expected = []
+
+    for p_row in p:
+        combined_mean = np.dot(p_row, component_means)
+        combined_variance = np.dot(
+            p_row, component_variances + (combined_mean - component_means) ** 2
+        )
+        combined_std_dev = np.sqrt(combined_variance)
+
+        mean_expected.append(combined_mean)
+        std_dev_expected.append(combined_std_dev)
+
+    gain_expected = torch.tensor(mean_expected) - 1.0
+    std_dev_expected = torch.tensor(std_dev_expected)
+
+    print("gain_expected: ", gain_expected)
+    print("gain_test: ", gain_test)
+    print("std_dev_expected: ", std_dev_expected)
+    print("std_dev_test: ", std_dev_test)
+
+    assert torch.allclose(gain_test, gain_expected, rtol=1e-4)
+    assert torch.allclose(std_dev_test, std_dev_expected, rtol=1e-4)
+
+
+def test_ensure_nonsingular():
+    """Check that ensure_nonsingular() on a specific test case."""
+    base_s = torch.tensor(
+        [
+            [1, 0.0, 0.0, 0.0, 0.0],
+            [0.75, -1.0, 0.0, 0.0, 0.0],
+            [0.5, 0.5, 1e-10, 0.0, 0.0],
+            [0.5, 0.5, 0.7, -1e-8, 0.0],
+            [0.5, 0.5, -1.0, 0.0, 0.0],
+        ]
+    )
+    s = base_s.unsqueeze(0).unsqueeze(0)
+    modified_s = loss_functions.ensure_nonsingular(s)
+    new_diagonal = torch.diagonal(modified_s, dim1=2, dim2=3).squeeze(0).squeeze(0)
+    delta = (torch.diagonal(modified_s - s, dim1=2, dim2=3)).squeeze(0).squeeze(0)
+    print(new_diagonal)
+
+    # Check both the direction of the change and the magnitude of any modified values:
+
+    # rows 0 and 1 should not change
+    assert delta[0] == 0.0
+    assert delta[1] == 0.0
+
+    # The diagonal entries in rows 2 and 3 should move in the same
+    # direction as their original sign
+    assert delta[2] > 0.0
+    assert delta[3] < 0.0
+
+    # The zero entry in row 4 should end up positive
+    assert delta[4] > 0.0
+
+    # The new values should have the same magnitude
+    assert abs(new_diagonal[2]) == abs(new_diagonal[3])
+    assert abs(new_diagonal[2]) == abs(new_diagonal[4])
