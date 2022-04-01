7df8eda83dd433f362f1b5f5de5ae6f2b095d467
Mark Wicks <mawicks@gmail.com>
2022-04-01 00:18:17 -0400

    Add doctest for YFinanceSource since a real test isn't a unit test.

--- data_sources.py
+++ data_sources.py
@@ -14,13 +14,28 @@ logging.basicConfig(level=logging.INFO)
 
 
 class YFinanceSource(object):
+    """
+    xxx
+    >>> import data_sources
+    >>> symbols = ["SPY", "QQQ"]
+    >>> ds = data_sources.YFinanceSource()
+    >>> response = ds.price_history(symbols)
+    >>> response["SPY"][:4][['open', 'close']]  # doctest: +NORMALIZE_WHITESPACE
+                    open     close
+    date
+    1993-02-01  43.96875  44.25000
+    1993-02-02  44.21875  44.34375
+    1993-02-03  44.40625  44.81250
+    1993-02-04  44.96875  45.00000
+        >>>
+    """
+
     @staticmethod
     def _add_columns(df):
         new_df = df.dropna().reset_index()
         rename_dict = {c: utils.rename_column(c) for c in new_df.columns}
         log_return = np.log(new_df["Adj Close"] / new_df["Adj Close"].shift(1))
         new_df = new_df.assign(log_return=log_return)
-        print(rename_dict)
         new_df.rename(columns=rename_dict, inplace=True)
         new_df.set_index("date", inplace=True)
         return new_df
@@ -33,7 +48,9 @@ class YFinanceSource(object):
         symbols = utils.to_symbol_list(symbol_set)
 
         # Do the download
-        df = yf.download(symbols, period="max", group_by="ticker", actions=True)
+        df = yf.download(
+            symbols, period="max", group_by="ticker", actions=True, progress=False
+        )
         response = {}
 
         for symbol in symbols:
