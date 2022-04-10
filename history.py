93467500b05aa9638f1241519712e2f938e83289
Mark Wicks <mawicks@gmail.com>
2022-04-09 20:20:39 -0400

    Add work on documentation of history.py

--- history.py
+++ history.py
@@ -1,10 +1,9 @@
 import logging
 import os
-from typing import Dict, Iterable, Union
+from typing import Callable, Dict, Iterable, Union
 
 # Third party libraries
 import pandas as pd
-import yfinance as yf
 
 # Local imports
 import data_sources
@@ -15,23 +14,65 @@ logging.basicConfig(level=logging.INFO)
 
 
 class FileSystemStore(object):
+    """
+    This clsss implements an abstract interface for data storage.  It
+    implements three methods:
+        exists() to determine whether an object has beenstored
+        write() to store an object
+        load() to load an object.
+    This particular implementation is specific to writing and loading
+    dataframes.  It does some additional housekeeping and sanity checking on the dataframe.
+
+    Abstracting this interface allows the file system to be replaced or
+    mocked out more esily for testing.
+    """
+
     def __init__(self, cache_dir="."):
         self.cache_dir = cache_dir
         os.makedirs(cache_dir, exist_ok=True)
 
     def _path(self, symbol: str) -> str:
+        """Construct a filesystem path to store and retrieve the data for the
+        associated givwn key
+        Arguments:
+            symbol: str
+        Returns:
+            str - The filesystem path to be used for the key
+        """
+        # TODO: Return a path object rather than a string to increase porability.
         symbol_path = os.path.join(self.cache_dir, f"{symbol.lower()}.csv")
         return symbol_path
 
     def exists(self, symbol: str) -> bool:
+        """Return whether the symbol exists in the data store
+        Arguments:
+            symbol: str - the symbol or key to retrieve
+        Returns:
+            True if the key exists in the data store.
+        """
         return os.path.exists(self._path(symbol))
 
     def write(self, symbol: str, df: pd.DataFrame):
+        """
+        Write a key and data (must be a dataframe) to the data store
+        Arguments:
+            symbol: str - The symbol or "key" for the data.
+            df: pd.DataFrame - The dataframe to store for that symbol.
+        Returns:
+            None
+        """
         df = df.reset_index().set_index("date")
         df.sort_index(inplace=True)
         df.to_csv(self._path(symbol), index=True)
 
     def load(self, symbol: str) -> pd.DataFrame:
+        """
+        Load a dataframe given its symbol.
+        Arguments:
+            symbol: str
+        Returns:
+            pd.DataFrame - The associated dataframe.
+        """
         df = pd.read_csv(
             self._path(symbol),
             index_col="date",
@@ -41,10 +82,34 @@ class FileSystemStore(object):
         return df
 
 
-def CachingDownloader(data_source, data_store):
+def CachingDownloader(
+    data_source: Callable[[Union[str, Iterable[str]]], Dict[str, pd.DataFrame]],
+    data_store: FileSystemStore,
+):
+    """
+    Construct and return a download function that will download and write the
+    results to the data store as necessary.
+    Arguments:
+        data_source: Callable[[Union[str, Iterable[str]]], Dict[str,
+        pd.DataFrame]] -  A datasource function which given a list of symbols
+        returns a dictionary keyed by the symbol with values that are dataframe with history data for
+        that symbol.
+
+        data_store: FilesystemStore (or similar) - An implementation of a data_store class (see
+        FileSystemStore above)
+
+    """
+
     def download(
         symbols: Union[Iterable[str], str], overwrite_existing: bool = False
     ) -> Dict[str, pd.DataFrame]:
+        """
+        Arguments:
+            symbols: Union[Iterable[str], str] - A symbol of list of symbols to populate
+        in the cache.
+            overwrite_existing: bool - Forces all symbols to be downloaded whether or not
+            they already exist in the cache.
+        """
         # Handle the case where `symbol`is a single symbol
         symbols = utils.to_symbol_list(symbols)
 
@@ -77,31 +142,31 @@ def CachingLoader(data_source, data_store):
 
     def load(symbols: Union[Iterable[str], str], overwrite_existing=False) -> None:
         """
-        Return a dataframe containing all historic values for the given set of symbosl.
-        The dates are inner joined so there is one row for each date where all symbols
-        have a value for that date.  The row index for the returned dataframe is the
-        date.  The column is a muli-level index where the first position is the symbol
-        and the second position is the value of interest (e.g., "close", "log_return", etc.)
+                Return a dataframe containing all historic values for the given set of symbosl.
+                The dates are inner joined so there is one row for each date where all symbols
+                have a value for that date.  The row index for the returned dataframe is the
+                date.  The column is a muli-level index where the first position is the symbol
+                and the second position is the value of interest (e.g., "close", "log_return", etc.)
 
-        The expected use case is to get the log returns for a portfolio of stocks.  For example,
-        the following returns a datafram of log returns for a portfolio on the dates where every
-        item in the portfolio has a return:
+                The expected use case is to get the log returns for a portfolio of stocks.  For example,
+                the following returns a datafram of log returns for a portfolio on the dates where every
+                item in the portfolio has a return:
 
-        df.loc[:, (symbol_list, 'log_return')]
+                df.loc[:, (symbol_list, 'log_return')]
 
-        This is intended for a portfolio, but you can specify just one stock if that's all that's required:
+                This is intended for a portfolio, but you can specify just one stock if that's all that's required:
 
-        df.loc[:, (symbol, 'log_return')]
+                df.loc[:, (symbol, 'log_return')]
 
-        Arguments:
-           symbols:  Union[Iterable[str], str] - a list of symbols of interst
-           overwrite_existing: bool - whether to overwrite previously downloaded data (default False)
+                Arguments:
+                   symbols:  Union[Iterable[str], str] - a list of symbols of interst
+                   overwrite_existing: bool - whether to overwrite previously downloaded data (default False)
 
-        Returns
-           pd.DataFrame - The column is a muli-level index where the first position is the symbol
-        and the second position is the value of interest (e.g., "close", "log_return", etc.)
+                Returns
+                   pd.DataFrame - The column is a muli-level index where the first position is the symbol
+                and the second position is the value of interest (e.g., "close", "log_return", etc.)
 
-        """
+        <"""
         symbols = utils.to_symbol_list(symbols)
         caching_download(symbols, overwrite_existing)
 
