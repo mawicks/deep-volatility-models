17a05197dfd302e4eb57c6aff624e5903214b37a
Mark Wicks <mawicks@gmail.com>
2022-04-19 21:36:35 -0400

    Add initial versions of hyperopt_opt and optuna_opt.

---
+++ hyperopt_opt.py
@@ -0,0 +1,88 @@
+import logging
+
+from hyperopt import hp, tpe, fmin, Trials
+import numpy as np
+
+import train_univariate
+
+logging.basicConfig(level=logging.INFO)
+
+SYMBOLS = [
+    "bnd",
+    "edv",
+    "tyd",
+    "gld",
+    "vnq",
+    "vti",
+    "spy",
+    "qqq",
+    "qld",
+    "xmvm",
+    "vbk",
+    "xlv",
+    "fxg",
+    "rxl",
+    "fxl",
+    "ibb",
+    "vgt",
+    "iyf",
+    "xly",
+    "uge",
+    "jnk",
+]
+
+search_space = {
+    "mixture_components": 3 + hp.randint("mixture_components", 4),
+    "feature_dimension": 40 + hp.randint("feature_dimension", 51),
+    "embedding_dimension": 3 + hp.randint("embedding_dimension", 13),
+    "gaussian_noise": hp.loguniform("gaussian_noise", np.log(1e-4), np.log(1e-2)),
+    "dropout": hp.uniform("dropout", 0, 0.25),
+    "learning_rate": hp.loguniform("learning_rate", np.log(1e-4), np.log(1e-2)),
+    "weight_decay": hp.loguniform("weight_decay", np.log(1e-8), np.log(1e-6)),
+    "window_size": hp.choice("window_size", [64, 256]),
+    "minibatch_size": 64 + hp.randint("minibatch_size", 193),
+}
+
+
+def objective(parameters):
+    # Be a good citizen and make a copy since we're going to modify the dictionary
+    parameters = parameters.copy()
+
+    # `minibatch_size` has to be a Python int, not a numpy int.
+    parameters["minibatch_size"] = int(parameters["minibatch_size"])
+
+    logging.info("************************")
+    for key, value in parameters.items():
+        logging.info(f"{key}: {value}")
+
+    loss = train_univariate.run(
+        model_file=None,
+        symbol=SYMBOLS,
+        refresh=False,
+        tune_embeddings=False,
+        just_embeddings=False,
+        max_epochs=400,
+        early_termination=20,
+        **parameters,
+    )
+
+    logging.info(f"loss: {loss}")
+    logging.info("************************")
+
+    return loss
+
+
+trials = Trials()
+
+best = fmin(
+    objective,
+    space=search_space,
+    algo=tpe.suggest,
+    max_evals=160,
+    trials=trials,
+)
+print(trials.trials)
+
+print("\n***** Best parameters *****")
+
+print(best)
---
+++ optuna_opt.py
@@ -0,0 +1,84 @@
+import logging
+
+import optuna
+
+import train_univariate
+
+logging.basicConfig(level=logging.INFO)
+
+SYMBOLS = [
+    "bnd",
+    "edv",
+    "tyd",
+    "gld",
+    "vnq",
+    "vti",
+    "spy",
+    "qqq",
+    "qld",
+    "xmvm",
+    "vbk",
+    "xlv",
+    "fxg",
+    "rxl",
+    "fxl",
+    "ibb",
+    "vgt",
+    "iyf",
+    "xly",
+    "uge",
+    "jnk",
+]
+
+
+def objective(trial):
+    mixture_components = trial.suggest_int("mixture_components", 1, 5)
+    feature_dimension = trial.suggest_int("feature_dimension", 5, 50)
+    embedding_dimension = trial.suggest_int("embedding_dimension", 3, 15)
+    gaussian_noise = trial.suggest_float("gaussian_noise", 1e-5, 1e-2, log=True)
+    dropout = trial.suggest_uniform("dropout", 0.0, 0.75)
+    learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True)
+    weight_decay = trial.suggest_float("weight_decay", 1e-9, 1e-5, log=True)
+    window_size = trial.suggest_categorical("window_size", [16, 64, 256])
+    minibatch_size = trial.suggest_int("minibatch_size", 32, 256)
+
+    logging.info("************************")
+    logging.info(f"mixture_components: {mixture_components}")
+    logging.info(f"feature_dimension: {feature_dimension}")
+    logging.info(f"embedding_dimension: {embedding_dimension}")
+    logging.info(f"gaussian_noise: {gaussian_noise}")
+    logging.info(f"dropout: {dropout}")
+    logging.info(f"learning_rate: {learning_rate}")
+    logging.info(f"weight_decay: {weight_decay}")
+    logging.info(f"window_size: {window_size}")
+    logging.info(f"minibatch_size: {minibatch_size}")
+
+    loss = train_univariate.run(
+        model_file=None,
+        symbol=SYMBOLS,
+        refresh=False,
+        tune_embeddings=False,
+        just_embeddings=False,
+        window_size=window_size,
+        mixture_components=mixture_components,
+        feature_dimension=feature_dimension,
+        gaussian_noise=gaussian_noise,
+        embedding_dimension=embedding_dimension,
+        minibatch_size=minibatch_size,
+        use_batch_norm=False,
+        dropout=dropout,
+        learning_rate=learning_rate,
+        weight_decay=weight_decay,
+    )
+
+    logging.info(f"loss: {loss}")
+    logging.info("************************")
+
+    return loss
+
+
+study = optuna.create_study()
+
+study.optimize(objective, n_trials=400)
+
+logging.info(f"{study.best_params}")
